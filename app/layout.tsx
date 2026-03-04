a33ca207d961be32cef736787778edb9301adfc0
josepeon <josepeon1803@gmail.com>
2026-03-04 16:37:08 -0500

    Set page title to Jose Peon, add white circle favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,7 +14,7 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "Project Showcase",
+  title: "Jose Peon",
   description: "A showcase of amazing projects",
 };
 
