3fc7ea532d6a49e3460e2a437659ea54f6a1135f
josepeon <josepeon1803@gmail.com>
2026-03-09 10:38:43 -0400

    add rewrite rules to proxy /parsons-homework to separate deployment

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,18 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {};
+const nextConfig: NextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: '/parsons-homework',
+        destination: 'https://parsons-homework.vercel.app/parsons-homework',
+      },
+      {
+        source: '/parsons-homework/:path*',
+        destination: 'https://parsons-homework.vercel.app/parsons-homework/:path*',
+      },
+    ];
+  },
+};
 
 export default nextConfig;
